3a6a5b01332b436a42ac9cfea62265e01591eea4
justinang1 <jya005@ucsd.edu>
2019-01-18 15:39:03 -0800

    Added working webpage with click to change image

--- web/html/js/scripts.js
+++ web/html/js/scripts.js
@@ -1,1 +1,11 @@
-$(document).ready();
+$(document).ready(function() {
+	let index = 1
+	const limit = 7
+	//alert(index);
+	$(".window").click(function(e) {
+		let newurl = `url(img/img_${index}.jpg)`
+		//alert(newurl)
+		$('.window').css('backgroundImage',newurl);
+		index = (index + 1) % limit 
+	});
+});
